889cda0d2dba8ef78dc2f21a81984b2cdb6f9c1a
m-elio <elio.musacchio28@gmail.com>
2023-04-20 02:09:59 +0200

    adapted ttest for exp1, exp2, exp3

--- src/evaluation/ttest.py
+++ src/evaluation/ttest.py
@@ -1,5 +1,5 @@
 """
-Module used both by `comparison` and `additional` experiment.
+Module used by `exp1`, `exp2` and `exp3` experiments.
 
 Computes the ttest statistical test, using AUC user wise results, for all models evaluated.
 """
@@ -11,10 +11,35 @@ from clayrs_can_see import evaluation as eva
 
 from src import REPORTS_DIR, ExperimentConfig
 
-
-def main_comparison():
+def perform_ttest(sys1_results, sys2_results, epoch: str, results_output_dir: str):
     """
-    Actual main function of the module for the `comparison` experiment.
+    Encapsulates the common operations carried out to perform the ttest statistical test on models trained using the
+    ClayRS framework.
+    The results of the evaluation procedure will be stored in dataframes and saved locally using the following formats:
+
+        * "ttest_{epoch}.csv"
+
+    Each result will be uniquely identified by the number of training epochs
+
+    Args:
+        sys1_results: dataframe containing the user-wise results for the first system
+        sys2_results: dataframe containing the user-wise results for the second system
+        epoch: number of training epoch used for the models associated to the results
+        results_output_dir: path to the directory where the results of the evaluation will be stored
+
+    """
+
+    result = eva.Ttest("user_idx").perform([sys1_results, sys2_results])
+
+    print(result, "\n")
+
+    result.to_csv(os.path.join(results_output_dir, f"ttest_{epoch}.csv"), index=False)
+    print(f"ttest results saved into {os.path.join(results_output_dir, f'ttest_{epoch}.csv')}!")
+
+
+def main_exp1():
+    """
+    Actual main function of the module for the `exp1` experiment.
 
     It will compute the ttest statistical test using ClayRS comparing, for each number of epochs used in the experiment,
     between ClayRS and Cornac user results.
@@ -26,33 +51,34 @@ def main_comparison():
     ttest_dir = os.path.join(REPORTS_DIR, 'ttest_results')
     os.makedirs(ttest_dir, exist_ok=True)
 
+    exp1_ttest_dir = os.path.join(ttest_dir, 'exp1')
+    os.makedirs(exp1_ttest_dir, exist_ok=True)
+
     print("".center(80, "-"))
     for epoch in ExperimentConfig.epochs:
         print(f"Considering number of epochs {epoch}")
         print("".center(80, "-"))
-        results_clayrs = pd.read_csv(os.path.join(REPORTS_DIR, "results_clayrs", f"users_results_clayrs_{epoch}.csv"))
-        results_cornac = pd.read_csv(os.path.join(REPORTS_DIR, "results_cornac", f"users_results_cornac_{epoch}.csv"))
+        result_clayrs = pd.read_csv(os.path.join(REPORTS_DIR, "exp1", "vbpr_clayrs",
+                                                 f"users_results_clayrs_imported_features_{epoch}.csv"))
+        result_cornac = pd.read_csv(os.path.join(REPORTS_DIR, "exp1", "vbpr_cornac",
+                                                 f"users_results_cornac_{epoch}.csv"))
 
-        result = eva.Ttest("user_idx").perform([results_clayrs, results_cornac])
-
-        print(result, "\n")
-
-        result.to_csv(os.path.join(ttest_dir, f"ttest_{epoch}.csv"), index=False)
-        print(f"ttest results saved into {os.path.join(ttest_dir, f'ttest_{epoch}.csv')}!")
+        perform_ttest(sys1_results=result_clayrs,
+                      sys2_results=result_cornac,
+                      epoch=epoch,
+                      results_output_dir=exp1_ttest_dir)
 
         # if this is the last epoch we do not print the separator
-        # pylint: disable=duplicate-code
         if epoch != ExperimentConfig.epochs[-1]:
             print("".center(80, '-'))
 
 
-def main_additional():
+def main_exp2():
     """
-    Actual main function of the module for the `additional` experiment.
+    Actual main function of the module for the `exp2` experiment.
 
     It will compute the ttest statistical test using ClayRS comparing, for each number of epochs used in the experiment,
-    all available representations user results in pairs ('resnet50' - 'caffe', 'resnet50' - 'caffe_center_crop'
-    and so on).
+    between ClayRS models trained on representations identified by ids `caffe` and `caffe_center_crop`.
 
     Results will be saved into `reports/ttest_results`.
 
@@ -61,26 +87,60 @@ def main_additional():
     ttest_dir = os.path.join(REPORTS_DIR, 'ttest_results')
     os.makedirs(ttest_dir, exist_ok=True)
 
-    repr_ids = ['resnet50', 'caffe', 'caffe_center_crop', 'vgg19']
-    new_index = [(repr_ids[j], repr_ids[i]) for j in range(len(repr_ids)) for i in range(j, len(repr_ids)) if i != j]
+    exp2_ttest_dir = os.path.join(ttest_dir, 'exp2')
+    os.makedirs(exp2_ttest_dir, exist_ok=True)
 
-    results_additional_exp_dir = os.path.join(REPORTS_DIR, "results_additional_exp")
-
+    print("".center(80, "-"))
     for epoch in ExperimentConfig.epochs:
-        results = [
-            pd.read_csv(os.path.join(results_additional_exp_dir, f"users_results_additional_exp_{repr_id}_{epoch}.csv"))
-            for repr_id in repr_ids]
+        print(f"Considering number of epochs {epoch}")
+        print("".center(80, "-"))
+        result_caffe = pd.read_csv(os.path.join(REPORTS_DIR, "exp2",
+                                                f"users_results_clayrs_caffe_{epoch}.csv"))
+        result_caffe_crop = pd.read_csv(os.path.join(REPORTS_DIR, "exp2",
+                                                     f"users_results_clayrs_caffe_center_crop_{epoch}.csv"))
 
-        result = eva.Ttest("user_idx").perform(results)
-        result.index = pd.Index(new_index)
-
-        print(result, "\n")
-
-        result.to_csv(os.path.join(ttest_dir, f"ttest_additional_exp_{epoch}.csv"), index=True)
-        print(f"ttest results saved into {os.path.join(ttest_dir, f'ttest_{epoch}.csv')}!")
+        perform_ttest(sys1_results=result_caffe,
+                      sys2_results=result_caffe_crop,
+                      epoch=epoch,
+                      results_output_dir=exp2_ttest_dir)
 
         # if this is the last epoch we do not print the separator
-        # pylint: disable=duplicate-code
+        if epoch != ExperimentConfig.epochs[-1]:
+            print("".center(80, '-'))
+
+
+def main_exp3():
+    """
+    Actual main function of the module for the `exp3` experiment.
+
+    It will compute the ttest statistical test using ClayRS comparing, for each number of epochs used in the experiment,
+    between ClayRS models trained on representations identified by ids `vgg19` and `resnet50`.
+
+    Results will be saved into `reports/ttest_results`.
+
+    """
+
+    ttest_dir = os.path.join(REPORTS_DIR, 'ttest_results')
+    os.makedirs(ttest_dir, exist_ok=True)
+
+    exp3_ttest_dir = os.path.join(ttest_dir, 'exp3')
+    os.makedirs(exp3_ttest_dir, exist_ok=True)
+
+    print("".center(80, "-"))
+    for epoch in ExperimentConfig.epochs:
+        print(f"Considering number of epochs {epoch}")
+        print("".center(80, "-"))
+        result_caffe = pd.read_csv(os.path.join(REPORTS_DIR, "exp3",
+                                                f"users_results_clayrs_vgg19_{epoch}.csv"))
+        result_caffe_crop = pd.read_csv(os.path.join(REPORTS_DIR, "exp3",
+                                                     f"users_results_clayrs_resnet50_{epoch}.csv"))
+
+        perform_ttest(sys1_results=result_caffe,
+                      sys2_results=result_caffe_crop,
+                      epoch=epoch,
+                      results_output_dir=exp3_ttest_dir)
+
+        # if this is the last epoch we do not print the separator
         if epoch != ExperimentConfig.epochs[-1]:
             print("".center(80, '-'))
 
@@ -88,7 +148,9 @@ def main_additional():
 if __name__ == "__main__":
 
     # pylint: disable=duplicate-code
-    if ExperimentConfig.experiment == "comparison":
-        main_comparison()
+    if ExperimentConfig.experiment == "exp1":
+        main_exp1()
+    elif ExperimentConfig.experiment == "exp2":
+        main_exp2()
     else:
-        main_additional()
+        main_exp3()
